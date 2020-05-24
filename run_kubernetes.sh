7f7b2af3ae83115deac8a26d7e21d5527b1e547e
Adorjan Szasz <ador.szasz@gmail.com>
2020-05-24 20:21:11 +0000

    kubernetes configs uploaded

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="adorszasz/udacity-nd9991-ml"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run --image=$dockerpath ml-app --port=80 --env="DOMAIN=cluster"
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward dockerpath 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,14 +6,15 @@
 # Step 1:
 # Create dockerpath
 localtag="ml-app"
-dockerpath="adorszasz/ml-app"
+dockerpath="adorszasz/udacity-nd9991-ml"
+tagname="ml-app"
 
 # Step 2:  
 # Authenticate & tag
-docker login --username=adorszasz --email=ador.szasz@gmail.com
-docker tag $localtag $dockerpath:firsttry
+docker login --username=adorszasz --password=V6k2nbhL3aUG8Q. docker.io
+docker tag $localtag $dockerpath:$tagname
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath
+docker push $dockerpath:$tagname
