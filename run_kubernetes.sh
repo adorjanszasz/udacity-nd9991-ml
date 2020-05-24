08841fc302ddac9df6224a1073af62858e04e63e
adorjanszasz <ador.szasz@gmail.com>
2020-05-24 23:07:26 +0200

    Kubernetes update
    Kubernetes config finally working;
    Test output uploaded

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath="adorszasz/udacity-nd9991-ml"
+dockerpath="adorszasz/udacity-nd9991-ml:ml-app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run ml-app --image=$dockerpath --port=80 --env="DOMAIN=cluster"
+kubectl run ml-apper --image=$dockerpath --env="DOMAIN=cluster" --env="POD_NAMESPACE=default"
 
 
 # Step 3:
