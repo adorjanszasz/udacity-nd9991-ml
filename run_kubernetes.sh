7cfd363d3f348ae28d180ef2cac0df23922c2d13
Adorjan Szasz <ador.szasz@gmail.com>
2020-05-24 20:23:05 +0000

    updates added

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath="adorszasz/udacity-nd9991-ml"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run --image=$dockerpath ml-app --port=80 --env="DOMAIN=cluster"
+kubectl run ml-app --image=$dockerpath --port=80 --env="DOMAIN=cluster"
 
 
 # Step 3:
@@ -17,4 +17,5 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward dockerpath 8000:80
+pod="kubectl get pods | awk '/ml-app/{print $1}' | head -n 1"
+kubectl port-forward $pod 8000:80
